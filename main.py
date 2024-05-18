99fd09dc44dd2dca90c8010eca0fa003443618f7
eylinem <eno@voima-technical.com>
2024-05-17 20:07:27 -0700

    add pages for each topic with nested for loop

--- main.py
+++ main.py
@@ -7,11 +7,14 @@ df = pd.read_csv("topics.csv")
 
 for index,row in df.iterrows():
     pdf.add_page()
-
     pdf.set_font(family="Arial", style="b", size=20)
     pdf.set_text_color(5,110,205)
     pdf.cell(w=0, h=.5, txt=row["Topic"], align="L", ln=1, border=0)
     pdf.set_draw_color(5,110,205)
     pdf.line(0.4,0.8,8.1,0.8)    #pdf.line(x1,y1,x2,y2)
+
+    for i in range(row["Pages"]-1):
+        pdf.add_page()
+
 
 pdf.output("output.pdf")
