f9835f9c8b04e9e0e585946db52552058b281d65
eylinem <eno@voima-technical.com>
2024-05-19 16:12:16 -0700

    added comments

--- txt_to_pdf.py
+++ txt_to_pdf.py
@@ -3,10 +3,12 @@ import pandas as pd
 import glob
 from pathlib import Path
 
+# Create list of filepaths of text files
 filepaths = glob.glob("Text_Files/*.txt")
-
+# Create one pdf document
 pdf = FPDF(orientation='P', unit='in', format='letter')
 
+# Go through each text file and create headers from file names
 for filepath in filepaths:
     section_header = Path(filepath).stem.title()
     pdf.add_page()
@@ -14,4 +16,5 @@ for filepath in filepaths:
     pdf.cell(w=2, h=0.5, txt=section_header)
     print(section_header)
 
+# Save pdf file
 pdf.output(f"Text_Files/output_pdf.pdf")
